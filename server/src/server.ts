46976ad72f18b3cf75c91fc0081b79ee498c9b08
Paulo Pires <prpires66@gmail.com>
2023-05-18 00:08:22 -0300

    Aula 01 - parcial - 49:33

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,18 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
-const app = fastify();
+const app = fastify()
+const prisma = new PrismaClient()
 
-app.get("/hello", () => {
-  return "Hello World";
-});
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+  return users
+})
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log("✔ HTTP Server running on http://localhost:3333");
-  });
+    console.log('✔ HTTP Server running on http://localhost:3333')
+  })
