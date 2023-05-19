77ec477a03ea97050e0c8c7a7ce5979eb20a41c6
Paulo Pires <prpires66@gmail.com>
2023-05-19 19:06:03 -0300

    Aula 02 - final web - 1:17:20

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/Button";
+
 export default function Home() {
   return (
-    <h1>
-      Hello World
-    </h1>
+<div className="h-screen bg-zinc-950 text-zinc-50 p-6">
+  <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+</div>
   )
 }
